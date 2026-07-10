a31316a06e9b18c6c92df3eef386ff0a35448d21
LucaOverflow <luca@pschl.net>
2026-07-10 13:23:14 +0200

    Add log in footer

--- frontend/src/lib/components/app-sidebar.svelte
+++ frontend/src/lib/components/app-sidebar.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
   import * as Sidebar from "$lib/components/ui/sidebar/index"
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index"
   import {
     Calendar03Icon,
     CalendarClockIcon,
+    ChevronUpIcon,
     InternetAntenna02Icon,
     RankingIcon,
     StopWatchIcon,
-    Timer02Icon,
-    UserAccountIcon } from '@hugeicons/core-free-icons';
+    Timer02Icon } from '@hugeicons/core-free-icons';
   import { HugeiconsIcon, type IconSvgElement } from '@hugeicons/svelte';
 
   interface MenuItem {
@@ -31,11 +32,6 @@
       title: "Decoder",
       url: "#",
       icon: StopWatchIcon
-    },
-    {
-      title: "Personal Data",
-      url: "#",
-      icon: UserAccountIcon
     }
   ]
 
@@ -58,36 +54,17 @@
   ]
 
   const isEventSelected = false // TODO Make dynamic
+  const isLoggedIn = false // TODO Make dynamic
 </script>
 
 <Sidebar.Root>
 
-  <Sidebar.Group>
-    <Sidebar.GroupLabel>Personal</Sidebar.GroupLabel>
-    <Sidebar.GroupContent>
-      <Sidebar.Menu>
-        {#each driverItems as item (item.title)}
-          <Sidebar.MenuItem>
-            <Sidebar.MenuButton>
-              {#snippet child({ props })}
-                <a href={item.url} {...props}>
-                    <HugeiconsIcon icon={item.icon} />
-                    <span>{item.title}</span>
-                  </a>
-              {/snippet}
-            </Sidebar.MenuButton>
-          </Sidebar.MenuItem>
-        {/each}
-      </Sidebar.Menu>
-    </Sidebar.GroupContent>
-  </Sidebar.Group>
-
-  {#if isEventSelected}
+  <Sidebar.Content>
     <Sidebar.Group>
-      <Sidebar.GroupLabel>Event</Sidebar.GroupLabel> <!-- TODO Add Event Name. Always show currently running Event as default. -->
+      <Sidebar.GroupLabel>Personal</Sidebar.GroupLabel>
       <Sidebar.GroupContent>
         <Sidebar.Menu>
-          {#each eventItems as item (item.title)}
+          {#each driverItems as item (item.title)}
             <Sidebar.MenuItem>
               <Sidebar.MenuButton>
                 {#snippet child({ props })}
@@ -102,6 +79,73 @@
         </Sidebar.Menu>
       </Sidebar.GroupContent>
     </Sidebar.Group>
-  {/if}
+
+    {#if isEventSelected}
+      <Sidebar.Group>
+        <Sidebar.GroupLabel>Event</Sidebar.GroupLabel> <!-- TODO Add Event Name. Always show currently running Event as default. -->
+        <Sidebar.GroupContent>
+          <Sidebar.Menu>
+            {#each eventItems as item (item.title)}
+              <Sidebar.MenuItem>
+                <Sidebar.MenuButton>
+                  {#snippet child({ props })}
+                    <a href={item.url} {...props}>
+                        <HugeiconsIcon icon={item.icon} />
+                        <span>{item.title}</span>
+                      </a>
+                  {/snippet}
+                </Sidebar.MenuButton>
+              </Sidebar.MenuItem>
+            {/each}
+          </Sidebar.Menu>
+        </Sidebar.GroupContent>
+      </Sidebar.Group>
+    {/if}
+  </Sidebar.Content>
+
+  <Sidebar.Footer>
+    <Sidebar.Menu>
+        <Sidebar.MenuItem>
+          {#if isLoggedIn}
+            <DropdownMenu.Root>
+              <DropdownMenu.Trigger>
+                {#snippet child({ props })}
+                  <Sidebar.MenuButton
+                    {...props}
+                    class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                  >
+                    Username <!-- TODO Add real username -->
+                    <HugeiconsIcon icon={ChevronUpIcon} class="ms-auto" />
+                  </Sidebar.MenuButton>
+                {/snippet}
+              </DropdownMenu.Trigger>
+              <DropdownMenu.Content
+                side="top"
+                class="w-(--bits-dropdown-menu-anchor-width)"
+              >
+                <DropdownMenu.Item>
+                  <span>Account</span>
+                </DropdownMenu.Item>
+                <DropdownMenu.Item>
+                  <span>Sign out</span>
+                </DropdownMenu.Item>
+              </DropdownMenu.Content>
+            </DropdownMenu.Root>
+          {:else}
+            <Sidebar.Menu>
+              <Sidebar.MenuItem>
+                <Sidebar.MenuButton>
+                  {#snippet child({ props })}
+                    <a href="#" {...props}> <!-- TODO Open Log in Popup -->
+                      <span>Log in</span>
+                    </a>
+                  {/snippet}
+                </Sidebar.MenuButton>
+              </Sidebar.MenuItem>
+            </Sidebar.Menu>
+          {/if}
+        </Sidebar.MenuItem>
+      </Sidebar.Menu>
+  </Sidebar.Footer>
 
 </Sidebar.Root>
