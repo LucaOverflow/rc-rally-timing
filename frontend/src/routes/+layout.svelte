ac01d99386360a8d992f1806b814db535235a738
LucaOverflow <luca@pschl.net>
2026-07-12 15:39:51 +0200

    Add shadcn-svelte Sonner

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -4,12 +4,14 @@
 	import { ModeWatcher } from 'mode-watcher';
 	import * as Sidebar from '$lib/components/ui/sidebar/index';
 	import AppSidebar from '$lib/components/app-sidebar.svelte';
+	import { Toaster } from "$lib/components/ui/sonner"
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <ModeWatcher />
+<Toaster position="bottom-center" />
 
 <Sidebar.Provider>
 	<AppSidebar />
