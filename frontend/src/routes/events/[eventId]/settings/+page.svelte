45d62ed0fe9e99f42fc291185869f3bb4008ad53
LucaOverflow <luca.peschel@gmail.com>
2026-09-01 21:37:56 +0200

    Added save button for event settings page

--- frontend/src/routes/events/[eventId]/settings/+page.svelte
+++ frontend/src/routes/events/[eventId]/settings/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import EventSettings from '$lib/components/event-settings.svelte';
   import { globals } from '$lib/globals.svelte';
+  import { Button } from "$lib/components/ui/button"
+  import { pb } from '$lib/pb';
+  import { toast } from 'svelte-sonner';
 
   let eventFormData = $state({
     name: globals.activeEvent?.name,
@@ -14,8 +17,27 @@
     max_drivers: globals.activeEvent?.max_drivers,
     allow_transponder_signup: globals.activeEvent?.allow_transponder_signup
     })
+
+  function saveSettings () {
+    if (globals.activeEvent == undefined) {
+      toast.error("Couldn't save event settings. Please re-enter the event from the event selection.")
+      return
+    }
+
+    pb.collection("events").update(globals.activeEvent.id, {
+      ...eventFormData
+    })
+      .then((updatedEvent) => {
+        toast.success("Event settings saved.")
+        globals.activeEvent = updatedEvent
+      })
+      .catch(() => {
+        toast.error("Couldn't save event settings.")
+      })
+  }
 </script>
 
 <div class="m-6">
   <EventSettings bind:formData={eventFormData} />
+  <Button onclick={saveSettings} class="mt-4">Save</Button>
 </div>
